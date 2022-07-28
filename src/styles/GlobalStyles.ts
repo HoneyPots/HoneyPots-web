e4852c6ef821474ebfca8564f87a9d8d35d14d19
daksae98 <hskim0610@gmail.com>
2022-07-28 22:32:52 +0900

    Update GlobalStyles.ts
    font-family

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -34,6 +34,7 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
   	line-height: 1;
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif ;
   }
   ol, ul {
   	list-style: none;
