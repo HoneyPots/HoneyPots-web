02c2513332a59976d18445f78ef7eaf08aed2515
qudwnbj <walpnv@gmail.com>
2022-08-07 13:56:05 +0900

    로그인쪽

--- src/screens/auth/AuthGuard.tsx
+++ src/screens/auth/AuthGuard.tsx
@@ -1,23 +1,39 @@
 import { useRouter } from 'next/router';
-import React, { FC, PropsWithChildren, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { ReduxState } from 'libs/store';
-import { UserState } from 'libs/store/modules/user';
+import React, { FC, PropsWithChildren, useEffect, useState } from 'react';
+import { HeadersDefaults } from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'libs/store/modules';
+import { userActions } from 'libs/store/modules/user';
+import axios from 'libs/axios';
 
 const AuthGuard: FC<PropsWithChildren> = ({ children }) => {
-  const { accessToken } = useSelector<ReduxState, UserState>(({ user }) => user);
+  const [loading, setLoading] = useState<boolean>(true);
+  const accessToken = useSelector<RootState, string>(({ user }) => user.accessToken);
   const router = useRouter();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!accessToken) {
-      router.push('/auth/login');
+    if (loading) {
+      // refresh token => access token
+      const getToken = () =>
+        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiaWF0IjoxNjU5NjE1ODg5LCJleHAiOjE2NTk3MDIyODl9.1HAH2sXJBEI1JXoC6Hv7a3_x2smxI27BHmPmzL0wzAq_QJjRb93IofxRc_hpqOkQMeE_zELSRKm6ZrvQs4I5aw';
+      const newToken = getToken();
+      dispatch(userActions.setToken(newToken));
+      axios.defaults.headers = {
+        ...axios.defaults.headers,
+        Authorization: getToken,
+      } as HeadersDefaults;
+      setLoading(false);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
-  if (accessToken || router.pathname.startsWith('/auth')) return <div>{children}</div>;
+    if (!loading && !accessToken) {
+      router.replace('/auth/login');
+    }
+  }, [accessToken, router, loading, dispatch]);
 
-  return <div />;
+  if (!loading || accessToken || router.pathname.startsWith('/auth')) return <div>{children}</div>;
+
+  return <div>Loading</div>;
 };
 
 export default AuthGuard;
