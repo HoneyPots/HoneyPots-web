7e5ea5802c034d10eb28bb61f34006a413b70504
daksae98 <hskim0610@gmail.com>
2022-08-13 18:40:53 +0900

    뜨거운수정

--- src/screens/groupBuying/search/GroupBuyingSearchView.tsx
+++ src/screens/groupBuying/search/GroupBuyingSearchView.tsx
@@ -8,60 +8,7 @@ export interface GroupBuyingSearchProps {
 }
 
 const GroupBuyingSearch: FC<GroupBuyingSearchProps> = ({ onClick, searchViewProps }) => (
-  <SearchView {...searchViewProps}>
-    <GroupBuyingPost
-      type="양식"
-      content="미스터 피자 같이 시켜 드실분 계실까요 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 계실까요? 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분"
-      postId={1}
-      title="미스터 피자 같이 시켜 드실분 "
-      uploadedAt={new Date('2022-7-31').toString()}
-      writer={{
-        id: 0,
-        nickname: '헤리움 318호',
-      }}
-      hasImage
-      onClick={onClick}
-    />
-    <GroupBuyingPost
-      type="양식"
-      content="미스터 피자 같이 시켜 드실분 계실까요 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 계실까요? 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분"
-      postId={1}
-      title="미스터 피자 같이 시켜 드실분 "
-      uploadedAt={new Date('2022-7-31').toString()}
-      writer={{
-        id: 0,
-        nickname: '헤리움 318호',
-      }}
-      hasImage
-      onClick={onClick}
-    />
-    <GroupBuyingPost
-      type="양식"
-      content="미스터 피자 같이 시켜 드실분 계실까요 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 계실까요? 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분"
-      postId={1}
-      title="미스터 피자 같이 시켜 드실분 "
-      uploadedAt={new Date('2022-7-31').toString()}
-      writer={{
-        id: 0,
-        nickname: '헤리움 318호',
-      }}
-      hasImage
-      onClick={onClick}
-    />
-    <GroupBuyingPost
-      type="양식"
-      content="미스터 피자 같이 시켜 드실분 계실까요 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 계실까요? 카카오톡 오픈 채팅 팠습니다. 들어오셔서 이야기 하시죵. 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분 미스터 피자 같이 시켜 드실분"
-      postId={1}
-      title="미스터 피자 같이 시켜 드실분 "
-      uploadedAt={new Date('2022-7-31').toString()}
-      writer={{
-        id: 0,
-        nickname: '헤리움 318호',
-      }}
-      hasImage
-      onClick={onClick}
-    />
-  </SearchView>
+  <SearchView {...searchViewProps} />
 );
 
 export default GroupBuyingSearch;
